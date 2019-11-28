b1ef9c95815047db60b24927b899f88e23392aaa
reira <reira.igg@gmail.com>
2019-11-28 17:19:55 +0800

    Updates

---
+++ precache-manifest.de89a582a69a2f7ef567507b75916ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eda8314eee21c7dcfab524d35b7321d",
+    "url": "/react-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "1fe57d6cd4f0d03fe738",
+    "url": "/react-gh-pages-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "75a1cf92e9a4d4146751",
+    "url": "/react-gh-pages-demo/static/js/2.e6557753.chunk.js"
+  },
+  {
+    "revision": "1fe57d6cd4f0d03fe738",
+    "url": "/react-gh-pages-demo/static/js/main.319644f4.chunk.js"
+  },
+  {
+    "revision": "5b4c1aa33ea8df9438d9",
+    "url": "/react-gh-pages-demo/static/js/runtime-main.fe83e0cf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages-demo/static/media/logo.25bf045c.svg"
+  }
+]);
